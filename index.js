a606cd55d0514483ef2040e4211cedd6b64e92ff
Lizaga Anadon <clizaga@147dda81acf0.ant.amazon.com>
2022-11-17 16:10:46 +0100

    add data layer

--- index.js
+++ index.js
@@ -1,49 +1,61 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const AWS = require("aws-sdk");
 
+AWS.config.update({ region: region });
+
+const ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
 const app = express();
 const port = process.env.PORT || 8000;
-const environment = process.env.ENVIRONMENT ?
-    process.env.ENVIRONMENT :
-    "not informed";
 
-console.log('ENVIRONMENT: ', environment);
+const environment = process.env.ENVIRONMENT
+  ? process.env.ENVIRONMENT
+  : "not informed";
+const table = process.env.TABLE ? process.env.TABLE : "environment";
+const region = process.env.REGION ? process.env.REGION : "us-east-1";
 
-const data = [{id: 1, attribute: "test1" }, {id: 2, attribute: "test2" }];
+console.log("ENVIRONMENT: ", environment);
+console.log("TABLE: ", table);
+console.log("REGION: ", region);
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    console.info('root route');
-    res.send(process.env.API_WORKS_MESSAGE);
+app.get("/", (req, res) => {
+  console.info("root route");
+  res.send(process.env.API_WORKS_MESSAGE);
 });
 
-app.get('/api/environment', (req, res) => {
-    console.info(`environment ${environment}`);
-    res.json([{
-        environment: `${environment}`
-    }]);
+app.get("/api/environment", (req, res) => {
+  console.info("environment data route");
+  res.json([
+    {
+      environment: `${environment}`,
+    },
+  ]);
 });
 
-app.get('/api/test', (req, res) => {
-    console.info('api test route');
-    res.json(data);
-});
+app.get("/api/data", (req, res) => {
+  console.info("api data route");
+  let params = {
+    TableName: table,
+    Key: {
+      environment: { N: environment },
+    },
+    ProjectionExpression: "createdAt",
+  };
 
-app.post('/api/test', (req, res) => {
-    console.info(`post`);
-    data.push(req.body);
-    res.json(data);
-});
-
-app.get('/api/test/:attribute', (req, res) => {
-    console.info(`filter by attribute:${req.params.attribute}`);
-    res.json(data.filter(x => req.params.attribute === x.attribute));
+  ddb.getItem(params, function (err, data) {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(data.Item);
+    }
+  });
 });
 
 app.listen(port, (err) => {
-    if (err) {
-        console.error('Error:', err);
-    }
-    console.info(`listening port ${port}`);
+  if (err) {
+    console.error("Error:", err);
+  }
+  console.info(`listening port ${port}`);
 });
